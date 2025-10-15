a734f1b44cb0a99e97ce2b66fd51310ecce31fcf
austin-i <austinivantsiv9@gmail.com>
2025-10-15 16:07:49 +0200

    Updated readme

---
+++ include/vinstr.hh
@@ -0,0 +1,16 @@
+// by Austin
+
+#ifndef VINSTR_HH
+#define VINSTR_HH
+
+
+class vinstr {
+public:
+    vinstr();
+
+// private:
+
+};
+
+
+#endif // INSTR_HH
---
+++ include/vreg.hh
@@ -0,0 +1,16 @@
+// by Austin
+
+#ifndef VREG_HH
+#define VREG_HH
+
+class vreg{
+public:
+    vreg();
+
+// private:
+
+};
+
+
+
+#endif // VREG_HH
